e546672db3ee4f54afd3c79120afce9898991f1c
osinko <yoshinari_m@msn.com>
2015-04-18 02:30:07 +0900

    no message

--- Assets/Sum2.cs
+++ Assets/Sum2.cs
@@ -1,26 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
 using System;
 
 public class Sum2 : MonoBehaviour
 {
+		void Start ()
+		{
+				print (Integral (4f, 0.01f, Gravity));
+				print (Integral (0.5f * Mathf.PI, 0.01f, Sin));
+		}
 
-	void Start ()
-	{
-		Func<float,float> f = Gravity;
-	}
+		float Integral (float t, float dt, Func<float,float> f)
+		{
+				float parts = t / dt;
+				float sum = 0;
 
-	float Integral (float upper, Func<float,float> f, float delta)
-	{
+				for (int n = 1; n <= parts; n++) {
+						sum += f (dt * n) * dt;
+				}
+				return sum;
+		}
 
-	}
+		float Gravity (float t)
+		{
+				return 9.8f * t;
+		}
 
-	float Gravity (float dt)
-	{
-		return 9.8f * dt;
-	}
-
-
-	
+		float Sin (float t)
+		{
+				return Mathf.Sin (t);
+		}
 }
